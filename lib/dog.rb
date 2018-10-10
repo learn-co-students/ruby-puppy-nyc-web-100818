354d258860733f98bb52edbf027e1859a41ed402
Austin Luft <ausluft@gmail.com>
2018-10-10 22:57:59 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,29 @@
+require "pry"
+
+class Dog
+
+attr_accessor :name
+
+@@all = []
+
+def self.all
+  @@all.map do |dog|
+    puts dog.name
+  end
+end
+
+def initialize(name)
+  @name = name
+  @@all << self
+end
+
+def self.clear_all
+  @@all.clear
+end
+
+
+
+
+
+
+end #class Dog
