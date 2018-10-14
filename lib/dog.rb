020dfd351da84d689aff7b846d8c34232a946694
magic8baller <lturis@gmail.com>
2018-10-14 00:50:22 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,28 +1,29 @@
+require 'pry'
 class Dog
 
   attr_accessor :name
-  #array << every puppy born right when instantiated!
+
   @@all = []
 
-  #array << every puppy born right when instantiated!
-
-
-  def initialize(name) #intialize w/ name arg
+  def initialize(name)
     @name = name
-    #self = new dog trying to store in array
     @@all << self
   end
 
-  #a class method that empties the @@all array of all existing dogs
+  def name
+    @name
+  end
+
+#this was hard!
+  def self.all
+    @@all.each do |dog|
+      puts "#{dog.name}"
+    end
+  end
+
   def self.clear_all
     @@all.clear
   end
 
-  #a class method that iterates over array of individual dogs in @@all & PUTS out the name of each dog
-  def self.all
-    @@all.each do |dog|
-      puts dog.name
-    end
-  end
 
 end
